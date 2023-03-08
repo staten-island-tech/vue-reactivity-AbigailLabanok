199005deaab6ef19ab0f7bbc085a27108da88a08
AbigailLabanok <abigail.labanok@gmail.com>
2023-03-08 13:16:28 -0500

    Update HomeView.vue

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -2,10 +2,36 @@
 import TheWelcome from "../components/TheWelcome.vue";
 </script>
 
-<template>
+<!-- <template>
   <main>
     <TheWelcome />
   </main>
 </template>
+ -->
+<template>
+  <div class="home">
+    <h1 v-if="graduated">{{ student }}</h1>
+    <h1 v-else>"They did not yet graduate"</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home",
+  components: {},
+  data() {
+    return {
+      student: "harry",
+      graduated: true,
+    };
+  },
+};
+</script>
+
+<style lang="css" scoped>
+h1 {
+  color: red;
+}
+</style>
 
 <!-- use vbase -->
