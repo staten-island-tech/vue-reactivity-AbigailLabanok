36415fc0e88ec725a30081283345988a6d57b687
AbigailLabanok <abigail.labanok@gmail.com>
2023-03-09 13:17:20 -0500

    Update HomeView.vue

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -10,8 +10,28 @@ import TheWelcome from "../components/TheWelcome.vue";
  -->
 <template>
   <div class="home">
-    <h1 v-if="graduated">{{ student }}</h1>
+    <!--         <h1 v-if="graduated">{{ student }}</h1>
     <h1 v-else>"They did not yet graduate"</h1>
+    <ul>
+      <li v-for="animal in animals" :key="animal">{{ animal }}</li>
+    </ul>
+    <button @click="authState" v-if="loggedIn">Log Out</button>
+    <button @click="authState" v-else>Login</button>
+    <input type="text" placeholder="edit me" v-model="message">
+    <p>{{ message }}</p>
+    <input type="checkbox" id="jack" value="jack" v-model="checkedNames">
+    <label for="jack">jack</label>
+    <input type="checkbox" id="john" value="john" v-model="checkedNames">
+    <label for="john">john</label>
+    <input type="checkbox" id="mike" value="mike" v-model="checkedNames">
+    <label for="mike">mike</label>
+    <span>checkedNames: {{ checkedNames }}</span> -->
+    <select v-model="selected">
+      <option>a</option>
+      <option>b</option>
+      <option>c</option>
+    </select>
+    <span>{{ selected }}</span>
   </div>
 </template>
 
@@ -21,9 +41,23 @@ export default {
   components: {},
   data() {
     return {
+      selected: "",
+      checkedNames: [],
       student: "harry",
       graduated: true,
+      animals: ["Pig", "horse", "donkey", "cow", "duck"],
+      loggedIn: false,
+      message: "",
     };
+  },
+  methods: {
+    authState: function () {
+      if (this.loggedIn === false) {
+        this.loggedIn = true;
+      } else {
+        this.loggedIn = false;
+      }
+    },
   },
 };
 </script>
@@ -34,4 +68,4 @@ h1 {
 }
 </style>
 
-<!-- use vbase -->
+/* use vbase */
