285bc1b19adb66e98e014480aa05ae54d46351e6
AbigailLabanok <abigail.labanok@gmail.com>
2023-03-21 13:17:46 -0400

    adasdasda

---
+++ vue/src/views/CartView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="cart">
+    <h1>CART</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .cart {
+    min-height: 100vh;
+  }
+}
+</style>
---
+++ vue/src/views/MenuView.vue
@@ -0,0 +1,5 @@
+<template>
+  <main>
+    <h1>MENU</h1>
+  </main>
+</template>
