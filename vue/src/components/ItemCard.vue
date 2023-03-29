be53e1d66acaa32be91bb088ac431535f5b14bcc
AbigailLabanok <abigail.labanok@gmail.com>
2023-03-29 13:18:24 -0400

    gjgh

---
+++ vue/src/components/ItemCard.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="card">
+      <h2>{{ title }}</h2>
+      <h3>{{ type }}</h3>
+      <img :src="image" alt="" />
+      <p>{{ price }}</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "ItemCard",
+    props: {
+      title: String,
+      type: String,
+      price: Number,
+      image: String,
+    },
+    computed: {
+      getImage: function () {
+        return this.image;
+      },
+    },
+  };
+  </script>
+  
+  <style>
+  .card {
+    width: 100%;
+    background-color: red;
+    height: 500px;
+    margin: 10px auto;
+  }
+  img {
+    height: 50%;
+  }
+  </style>
--- vue/src/views/MenuView.vue
+++ vue/src/views/MenuView.vue
@@ -5,33 +5,44 @@
 </template>
 
 <script>
+import ItemCard from "../components/ItemCard.vue";
 export default {
+  name: "main",
+  components: {
+    ItemCard,
+  },
   data() {
     return {
       ItemArr: [
         {
-          name: "Aero Phantom",
-          mainColor: "red",
+          name: "Game Master",
+          type: "Cheat",
           price: 1225,
-          img: "",
+          img: "https://media.contentapi.ea.com/content/dam/eacom/lost-in-random/images/2021/09/lir-game-master-card-image.png.adapt.crop16x9.652w.png",
         },
         {
-          name: "Spline Phantom",
-          mainColor: "blue",
+          name: "Explosive Dice",
+          type: "Damage",
           price: 1775,
-          img: "",
+          img: "https://media.contentapi.ea.com/content/dam/eacom/lost-in-random/images/2021/09/lir-game-explosive-dice-image.png.adapt.crop16x9.652w.png",
         },
         {
-          name: "Kingdom Phantom",
-          mainColor: "yellow",
+          name: "Ooma's Embrace",
+          type: "Defense",
           price: 935,
-          img: "",
+          img: "https://media.contentapi.ea.com/content/dam/eacom/lost-in-random/images/2021/09/lir-game-ooma-s-blessing-image.png.adapt.crop16x9.652w.png",
         },
         {
-          name: "Smite Phantom",
-          mainColor: "blue",
+          name: "Triplet Bombs",
+          type: "Hazard",
           price: 875,
-          img: "",
+          img: "https://media.contentapi.ea.com/content/dam/eacom/lost-in-random/images/2021/09/lir-game-triplet-bombs-image.png.adapt.crop16x9.652w.png",
+        },
+        {
+          name: "Hammer of Two",
+          type: "Weapon",
+          price: 875,
+          img: "https://media.contentapi.ea.com/content/dam/eacom/lost-in-random/images/2021/09/lir-game-hammer-of-two-image.png.adapt.crop16x9.652w.png",
         },
       ],
     };
